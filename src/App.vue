2b889fe6f3f55e9de9ea197fb04a2c507f2a7389
devKmee <82103@DESKTOP-OB7C880>
2023-05-23 00:59:27 +0900

    11 To-Do 카드 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,45 @@
 <template>
     <div class="container">
         <h2>TO-DO-LIST</h2>
-        <form 
-              class="d-flex"
-              @submit="onSubmit"
-        >
+        <form class="d-flex"
+              @submit="onSubmit">
             <div class="flex-grow-1" mr-2>
                 <input class="form-control"
                        type="text"
                        v-model="todo"
-                       placeholder="Type new to-do"/>
+                       placeholder="Type new to-do" />
             </div>
             <div>
                 <button class="btn btn-primary"
-                        type="submit"
-                >Add
+                        type="submit">
+                    Add
                 </button>
             </div>
         </form>
-        {{ todos }}
+        <div class="card mt-2">
+            <div class="card-body p-2">
+                {{ todos[0].subject }}
+            </div>
+        </div>
+        <div class="card mt-2">
+            <div class="card-body p-2">
+                {{ todos[1].subject }}
+            </div>
+        </div>
     </div>
 </template>                         
 
 <script>
-
     //import { reactive } from 'vue'; // 객체
     import { ref } from 'vue';    // 원시타입
 
     export default {
         setup() {
             const todo = ref('');
-            const todos = ref([]);
+            const todos = ref([
+                { id : 1, subject : '장보기' },
+                { id : 2, subject : '설거지' }
+            ]);
 
             const onSubmit = (e) => {
                 e.preventDefault();
