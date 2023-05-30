9e80dfc34ec9cd8fef8508c4c46b379b8f1cacac
devKmee <82103@DESKTOP-OB7C880>
2023-05-30 19:40:31 +0900

    24 검색 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="container">
-        <h4>count : {{ count }} </h4>
-        <h4>doubleCountComputed : {{ doubleCountComputed }} </h4>
-        <h4>doubleCountComputed : {{ doubleCountComputed }} </h4>
-        <h4>doubleCountMethod : {{ doubleCountMethod() }} </h4>
-        <h4>doubleCountMethod : {{ doubleCountMethod() }} </h4>
-        <button @click="count++">Add One</button>
         <h2>TO-DO-LIST</h2>
+        <input class="form-control"
+               type="text"
+               v-model="searchText"
+               placeholder="Search"
+        />
+        <hr />
         <TodoSimpleForm @add-todo="addTodo" />
 
-        <div v-if="!todos.length">
-            할 일을 추가해주세요
+        <div v-if="!filteredTodos.length">
+            There is nothing to display
         </div>
-        <TodoList :todos="todos"
+        <TodoList :todos="filteredTodos"
                   @toggle-todo="toggleTodo"
                   @delete-todo="deleteTodo" />
     </div>
@@ -20,7 +20,7 @@
 
 <script>
     //import { reactive } from 'vue'; // 객체
-    import { ref, computed } from 'vue';    // 원시타입
+    import { ref, computed} from 'vue';    // 원시타입
     import TodoSimpleForm from './components/TodoSimpleForm.vue';
     import TodoList from './components/TodoList.vue';
 
@@ -49,16 +49,15 @@
                 todos.value[index].completed = !todos.value[index].completed;
             };
 
-            const count = ref(1);
-            const doubleCountComputed = computed(() => {
-                console.log('computed');
-                return count.value * 2;
+            const searchText = ref('');
+            const filteredTodos = computed(()  => {
+                if (searchText.value) {
+                    return todos.value.filter(todo => {
+                        return todo.subject.includes(searchText.value);
+                    });
+                }
+                return todos.value;
             });
-
-            const doubleCountMethod = () => {
-                console.log('Method');
-                return count.value * 2;
-            };
 
             return {
                 addTodo
@@ -66,9 +65,8 @@
                 , todoStyle
                 , deleteTodo
                 , toggleTodo
-                , count
-                , doubleCountComputed
-                , doubleCountMethod
+                , searchText
+                , filteredTodos
             };
         }
     }
