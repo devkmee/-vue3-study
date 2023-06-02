d6e7c35ad4b005a5e110253a4369b865e8c5addb
pkm <pkmi@forcewave.co.kr>
2023-06-02 16:21:07 +0900

    33 Pagination 2

--- src/App.vue
+++ src/App.vue
@@ -21,13 +21,24 @@
         <hr>
         <nav aria-label="Page navigation example">
             <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                <li class="page-item">
+                    <a v-if="currentPage !== 1"
+                        class="page-link" href="#">Previous</a>
+                </li>
+                <li v-for="page in numberOfPages"
+                    :key="page"
+                    class="page-item"
+                    :class="currentPage === page ? 'active' : ''"
+                >
+                    <a class="page-link" href="#">{{ page }}</a>
+                </li>
+                <li class="page-item">
+                    <a v-if="currentPage != numberOfPages"
+                    class="page-link" href="#">Next</a>
+                </li>
             </ul>
         </nav>
+        {{ numberOfPages }}
     </div>
 </template>                    
 
@@ -46,16 +57,20 @@
         setup() {
             const todos = ref([]);
             const error = ref('');
-            const totalPage = ref(0);
+            const numberOfTodos = ref(0);
             const limit = 5;
-            const page = ref(1);
+            const currentPage = ref(1);
+
+            const numberOfPages = computed(() => {
+                return Math.ceil(numberOfTodos.value/limit);
+            });
 
             const getTodos = async () => {
                 try {
                     const res = await axios.get(
-                        `http://localhost:3000/todos?_page=${page.value}&_limit=${limit}`
+                        `http://localhost:3000/todos?_page=${currentPage.value}&_limit=${limit}`
                         );
-                    totalPage.value = res.headers['x-total-count'];
+                    numberOfTodos.value = res.headers['x-total-count'];
                     todos.value = res.data;                    
                 } catch (err) {
                     console.log(err);
@@ -135,6 +150,8 @@
                 , searchText
                 , filteredTodos
                 , error
+                , numberOfPages
+                , currentPage
             };
         }
     }
