5275ed64dc4b5ad06215028986c0cf77ce7553f2
devKmee <82103@DESKTOP-OB7C880>
2023-06-04 00:06:19 +0900

    38 작은 문제점들 수정

--- src/App.vue
+++ src/App.vue
@@ -82,7 +82,7 @@
                 currentPage.value = page;
                 try {
                     const res = await axios.get(
-                        `http://localhost:3000/todos?subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
+                        `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
                         );
                     numberOfTodos.value = res.headers['x-total-count'];
                     todos.value = res.data;                    
@@ -98,11 +98,11 @@
                 error.value = '';
                 try {
                     //DB에 저장
-                    const res = await axios.post('http://localhost:3000/todos', {
+                    await axios.post('http://localhost:3000/todos', {
                         subject: todo.subject,
                         completed: todo.completed
                     });
-                    todos.value.push(res.data);
+                    getTodos(1);
                 } catch (err) {
                     console.log(err);
                     error.value = 'Something went wrong';
@@ -124,7 +124,7 @@
                 const id = todos.value[index].id;
                 try {
                     await axios.delete('http://localhost:3000/todos/' + id);
-                    todos.value.splice(index, 1);
+                    getTodos(1);
                 } catch (err) {
                     console.log(err);
                     error.value = 'Something went wrong';
