e0e25804edaa0331a22701119f105e70bac1dc0e
devKmee <82103@DESKTOP-OB7C880>
2023-06-04 02:21:59 +0900

    42 Navbar 추가하기

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <template>
+    <!--네비게이션-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">devKmee</a>
+
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Todos <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </nav>
+
+    <!--라우터-->
     <router-view/>
 </template>                    
 
