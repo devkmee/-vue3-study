bc3eee44f9b1958037baa4f5409b662355878189
devKmee <82103@DESKTOP-OB7C880>
2023-06-04 02:12:50 +0900

    41 지금까지 만든 To-Do 앱을 To-Do 페이지로 이동

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,19 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory  } from 'vue-router';
 import Home from '../pages/index.vue';
 import Todos from '../pages/todos/index.vue';
 
-const router = createRouter ({
-    history: createWebHashHistory(),
-    //{객체} = 1개의 라우츠
+const router = createRouter({
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
-            name: 'HOME',
-            COMPONET: Home
+            name: 'Home',
+            component: Home
         },
         {
             path: '/todos',
             name: 'Todos',
-            COMPONET: Todos
+            component: Todos
         }
     ]
 });
