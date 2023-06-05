2b806b6320596ccc3b9ebae2d445ae17c7e333fb
pkm <pkmi@forcewave.co.kr>
2023-06-05 15:36:16 +0900

    67 slot

---
+++ src/components/DeleteModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <modal>
+    <template v-slot:title>Delete Todo!!!</template>
+    <template v-slot:body>Are you sure you want to delete the todo? tag도 넣을 수 있다.</template>
+    <template v-slot:footer>
+        <button 
+                type="button" 
+                class="btn btn-secondary" 
+                @click="onClose"
+            >
+                Close
+            </button>
+            <button type="button" class="btn btn-danger" @click="onDelete"
+            >Delete
+            </button>
+    </template>
+  </modal>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+    components: {
+        Modal
+    },
+    setup(props, { emit }) {
+        const onClose = () => {
+            emit('close');
+        }
+
+        const onDelete = () => {
+            emit('delete');
+        }
+
+        return {
+            onClose,
+            onDelete
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
