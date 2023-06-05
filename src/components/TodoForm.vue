9127febf4c731d8f9085fd3e2f7d1f31cce6bc78
pkm <pkmi@forcewave.co.kr>
2023-06-05 16:44:54 +0900

    69 컴포넌트에 v-model 사용하기

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -8,17 +8,9 @@
     >
       <div class="row">
         <div class="col-6">
-          <div class="form-group">
-            <label>Subject</label>
-            <input 
-              v-model="todo.subject" 
-              type="text" 
-              class="form-control"
-            >
-            <div v-if="subjectError" class="text-red"> 
-                {{ subjectError }}
-            </div>
-          </div>
+          <Input label="Subject" 
+                v-model:subject="todo.subject"
+                :error="subjectError"/>
         </div>
         <div v-if="editing" class="col-6">
           <div class="form-group">
@@ -69,13 +61,16 @@
   <script>
   import { useRoute, useRouter } from 'vue-router';
   import axios from 'axios';
-  import { ref, computed  } from 'vue';
+  import { ref, computed, onUpdated  } from 'vue';
   import _ from 'lodash';
   import Toast from '@/components/Toast.vue';
   import { useToast } from '@/composables/toast';
+  import Input from '@/components/input.vue';
+
   export default {
     components: {
-      Toast
+      Toast,
+      Input
     },
     props: {
         editing: {
@@ -91,6 +86,11 @@
               completed: false,
               body: ''
           });
+
+          onUpdated(() => {
+            console.log(todo.value.subject);
+          });
+
           const subjectError = ref('');
           const originalTodo = ref(null);
           const loading = ref(false);
@@ -100,7 +100,9 @@
             showToast,
             triggerToast
           } = useToast();
-          const todoId = route.params.id        
+
+          const todoId = route.params.id
+
           const getTodo = async () => {
               loading.value = true;
             try {
@@ -177,7 +179,7 @@
             showToast,
             toastMessage,
             toastAlertType,
-            subjectError
+            subjectError,
           };
       }
   }
---
+++ src/components/input.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="form-group">
+        <label>{{  label }}</label>
+        <input 
+            :value="subject"
+            @input="onInput"
+            type="text" 
+            class="form-control"
+        >
+        <div v-if="subjectError" class="text-red"> 
+            {{ error }}
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        error: {
+            type: String,
+            required: true
+        },
+        subject: {
+            type: String,
+            required: true
+        }
+    },
+    setup(props, { emit }){
+        const onInput = (event) => {
+            //컴포넌트에 올릴 이름 update:props속성이름
+            emit('update:subject', event.target.value)
+        }
+
+        return{
+            onInput
+        }
+    }
+}
+</script>
+
+<style scoped>
+  .text-red {
+    color: red;
+  }
+</style>
