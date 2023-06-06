daac7a8afb907b2eedf395924c8bc161ebd0496b
devKmee <82103@DESKTOP-OB7C880>
2023-06-06 22:52:38 +0900

    77 vuex mutations/actions

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,35 @@ export default createStore ({
         toastAlertType: '' ,
         showToast     : false ,
         timeout       : null 
+    },
+    mutations: {
+        UPDATE_TOAST_MESSAGE (state, payload) {
+            state.toastMessage = payload;
+        },
+        UPDATE_TOAST_ALERT_TYPE (state, payload) {
+            state.toastAlertType = payload;
+        },
+        UPDATE_TOAST_STATUS (state, payload) {
+            state.showToast = payload;
+        },
+        UPDATE_TOAST_TIMEOUT (state, payload) {
+            state.timeout = payload;
+        }
+    },
+    actions : {
+        triggerToast = ( { commit }, msg, type = 'success') => {
+            //toastMessage.value = msg;
+            //toastAlertType.value = type;
+            //showToast.value = true;           
+            commit('UPDATE_TOAST_MESSAGE', msg)
+            commit('UPDATE_TOAST_ALERT_TYPE', type)
+            commit('UPDATE_TOAST_STATUS', true)
+
+            //초기화
+            timeout.value = setTimeout( () => {
+                commit('UPDATE_TOAST_MESSAGE', '')
+                commit('UPDATE_TOAST_ALERT_TYPE', '')
+                commit('UPDATE_TOAST_STATUS', false)
+            }, 5000 )
     }
 })
